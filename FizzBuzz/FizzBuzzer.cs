cf4ec5c41d19c6037223ab2c93df9b35e58178d4
Jonathan Welch <jon@kidly.co.uk>
2023-07-20 11:07:56 +0100

    Ensured FizzBuzzer can return Buzz

--- FizzBuzz/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzzer.cs
@@ -4,6 +4,8 @@ public class FizzBuzzer : IFizzBuzzer
 {
     public string Generate(int number)
     {
+        if (number % 5 == 0) return "Buzz";
+
         return number % 3 == 0 ? "Fizz" : number.ToString();
     }
 }
