0e956993549a9ab5743cac8226f62d554c980db3
Jonathan Welch <jon@kidly.co.uk>
2023-07-20 11:11:09 +0100

    Included more test cases for Fizz and Buzz

--- FizzBuzzTests/Unit/FizzBuzzerTests.cs
+++ FizzBuzzTests/Unit/FizzBuzzerTests.cs
@@ -9,6 +9,10 @@ public class FizzBuzzerTests
     [TestCase("2", 2)]
     [TestCase("Fizz", 3)]
     [TestCase("Buzz", 5)]
+    [TestCase("Fizz", 6)]
+    [TestCase("Fizz", 9)]
+    [TestCase("Buzz", 10)]
+
     public void CanGenerateFizzBuzzFromNumber(string expected, int number)
     {
         var fizzBuzzer = new FizzBuzzer();
