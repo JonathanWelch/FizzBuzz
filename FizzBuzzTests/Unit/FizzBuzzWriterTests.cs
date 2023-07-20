673a79b78929cbc52517df1422a1655ca9bed611
Jonathan Welch <jon@kidly.co.uk>
2023-07-20 10:55:25 +0100

    Moved repeated code in FizzBuzzWriterTests to a SetUp method

--- FizzBuzzTests/Unit/FizzBuzzWriterTests.cs
+++ FizzBuzzTests/Unit/FizzBuzzWriterTests.cs
@@ -6,31 +6,36 @@ namespace FizzBuzzTests.Unit;
 [TestFixture]
 public class FizzBuzzWriterTests
 {
+    private Mock<TextWriter> _textWriterMock;
+    private Mock<IFizzBuzzer> _fizzBuzzerMock;
+    private FizzBuzzWriter _writer;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _textWriterMock = new Mock<TextWriter>();
+        _fizzBuzzerMock = new Mock<IFizzBuzzer>();
+        _writer = new FizzBuzzWriter(_textWriterMock.Object, _fizzBuzzerMock.Object);
+    }
+
     [Test]
     public void CanCollaborateWithFizzBuzzer()
     {
-        var textWriterMock = new Mock<TextWriter>();
-        var fizzBuzzerMock = new Mock<IFizzBuzzer>();
-        var writer = new FizzBuzzWriter(textWriterMock.Object, fizzBuzzerMock.Object);
+        _writer.Write(1, 2);
 
-        writer.Write(1, 2);
-
-        fizzBuzzerMock.Verify(buzzer => buzzer.Generate(1), Times.Once());
-        fizzBuzzerMock.Verify(buzzer => buzzer.Generate(2), Times.Once());
+        _fizzBuzzerMock.Verify(buzzer => buzzer.Generate(1), Times.Once());
+        _fizzBuzzerMock.Verify(buzzer => buzzer.Generate(2), Times.Once());
     }
 
     [Test]
     public void CanWriteFizzBuzzResultToWriter()
     {
-        var textWriterMock = new Mock<TextWriter>();
-        var fizzBuzzerMock = new Mock<IFizzBuzzer>();
-        fizzBuzzerMock.Setup(buzzer => buzzer.Generate(1)).Returns("1");
-        fizzBuzzerMock.Setup(buzzer => buzzer.Generate(2)).Returns("2");
-        var writer = new FizzBuzzWriter(textWriterMock.Object, fizzBuzzerMock.Object);
+        _fizzBuzzerMock.Setup(buzzer => buzzer.Generate(1)).Returns("1");
+        _fizzBuzzerMock.Setup(buzzer => buzzer.Generate(2)).Returns("2");
 
-        writer.Write(1, 2);
+        _writer.Write(1, 2);
 
-        textWriterMock.Verify(writer => writer.WriteLine("1"), Times.Once());
-        textWriterMock.Verify(writer => writer.WriteLine("2"), Times.Once());
+        _textWriterMock.Verify(writer => writer.WriteLine("1"), Times.Once());
+        _textWriterMock.Verify(writer => writer.WriteLine("2"), Times.Once());
     }
 }
