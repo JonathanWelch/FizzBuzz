d5eafd6787c2a7f4b3b97d8d959a0c8bef7536bf
Jonathan Welch <jon@kidly.co.uk>
2023-07-20 10:48:10 +0100

    Added logic to FizzBuzzWriter to make test pass.  Writer now collaborate with the FizzBuzzer.

--- FizzBuzz/FizzBuzzWriter.cs
+++ FizzBuzz/FizzBuzzWriter.cs
@@ -2,12 +2,18 @@
 
 public class FizzBuzzWriter
 {
+    private readonly IFizzBuzzer _fizzBuzzer;
+
     public FizzBuzzWriter(TextWriter writer, IFizzBuzzer fizzBuzzer)
     {
+        _fizzBuzzer = fizzBuzzer;
     }
 
     public void Write(int startValue, int endValue)
     {
-        
+        for (var i = startValue; i <= endValue; i++)
+        {
+            _fizzBuzzer.Generate(i);
+        }
     }
 }
