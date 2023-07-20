095fb2fca5401759ff20ef262aafd4aba9b76d0e
Jonathan Welch <jon@kidly.co.uk>
2023-07-20 10:58:19 +0100

    Added failing unit test for FizzBuzzer

---
+++ FizzBuzzTests/Unit/FizzBuzzerTests.cs
@@ -0,0 +1,17 @@
+﻿using FizzBuzz;
+
+namespace FizzBuzzTests.Unit;
+
+[TestFixture]
+public class FizzBuzzerTests
+{
+    [Test]
+    public void CanGenerateFizzBuzzFromNumber()
+    {
+        var fizzBuzzer = new FizzBuzzer();
+
+        var fizzBuzzed = fizzBuzzer.Generate(1);
+
+        Assert.That(fizzBuzzed, Is.EqualTo("1"));
+    }
+}
