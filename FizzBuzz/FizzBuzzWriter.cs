28b882d33c74c5b5a985eb8e703e63e559789e8c
Jonathan Welch <jon@kidly.co.uk>
2023-07-20 10:22:05 +0100

    Added Failing Acceptance test

---
+++ FizzBuzz/FizzBuzzWriter.cs
@@ -0,0 +1,13 @@
+﻿namespace FizzBuzz;
+
+public class FizzBuzzWriter
+{
+    public FizzBuzzWriter(TextWriter writer)
+    {
+    }
+
+    public void Write(int startValue, int endValue)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ FizzBuzzTests/Acceptance/FizzBuzzWriterTests.cs
@@ -0,0 +1,40 @@
+﻿using FizzBuzz;
+
+namespace FizzBuzzTests.Acceptance;
+
+[TestFixture]
+public class FizzBuzzWriterTests
+{
+    [Test]
+    public void CanWriteFizzBuzz()
+    {
+        var stringWriter = new StringWriter();
+        var fizzBuzzWriter = new FizzBuzzWriter(stringWriter);
+
+        fizzBuzzWriter.Write(1, 20);
+
+        const string expectedOutput = @"1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+Fizzbuzz
+16
+17
+Fizz
+19
+Buzz
+";
+
+        Assert.That(stringWriter.ToString(), Is.EqualTo(expectedOutput));
+    }
+}
