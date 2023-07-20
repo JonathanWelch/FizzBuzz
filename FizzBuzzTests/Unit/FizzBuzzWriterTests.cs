d8047e8b2759e6fb4a9064e0eb220896029aa5e1
Jonathan Welch <jon@kidly.co.uk>
2023-07-20 10:49:56 +0100

    Added failing unit test that asserts that the fizz buzz result is written to the text writter.

--- FizzBuzzTests/Unit/FizzBuzzWriterTests.cs
+++ FizzBuzzTests/Unit/FizzBuzzWriterTests.cs
@@ -18,4 +18,19 @@ public class FizzBuzzWriterTests
         fizzBuzzerMock.Verify(buzzer => buzzer.Generate(1), Times.Once());
         fizzBuzzerMock.Verify(buzzer => buzzer.Generate(2), Times.Once());
     }
+
+    [Test]
+    public void CanWriteFizzBuzzResultToWriter()
+    {
+        var textWriterMock = new Mock<TextWriter>();
+        var fizzBuzzerMock = new Mock<IFizzBuzzer>();
+        fizzBuzzerMock.Setup(buzzer => buzzer.Generate(1)).Returns("1");
+        fizzBuzzerMock.Setup(buzzer => buzzer.Generate(2)).Returns("2");
+        var writer = new FizzBuzzWriter(textWriterMock.Object, fizzBuzzerMock.Object);
+
+        writer.Write(1, 2);
+
+        textWriterMock.Verify(writer => writer.WriteLine("1"), Times.Once());
+        textWriterMock.Verify(writer => writer.WriteLine("2"), Times.Once());
+    }
 }
